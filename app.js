f008d50e5b04dfeee1e59d9ab1fa786eed423b7b
conseQnz <51886136+sammykINC@users.noreply.github.com>
2023-07-11 17:12:41 +0300

    outside click close

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const selectText = document.querySelector('.selectText');
 const listOption = document.getElementsByClassName('listOption');
 const smList = document.querySelector('.smList');
 const arrowIcon = document.querySelector('.arrowIcon');
+const hero = document.querySelector(".hero");
 
 for (selectedLi of listOption) {
   selectedLi.onclick = function() {
@@ -16,4 +17,11 @@ selectField.onclick = function() {
   smList.classList.toggle("hideList");
   smList.style.animation = "dropAnimation 0.3s ease-in-out";
   arrowIcon.classList.toggle("arrowRotation");
+}
+
+window.onclick = function(e) {
+  if (e.target == hero) {
+    smList.classList.toggle("hideList");
+    arrowIcon.classList.toggle("arrowRotation");
+  } 
 }
